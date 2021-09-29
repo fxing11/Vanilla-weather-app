a88b20bf04b914bfefde9370dda8ef77cf6c1d14
Fei Xing <56478202+fxing11@users.noreply.github.com>
2021-09-29 14:09:03 +0200

    added weather temperature and weather description function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
 
+let city_name = "Zurich";
+let apiKey = "48477574ab33f03aeefc4b00219c6195";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
